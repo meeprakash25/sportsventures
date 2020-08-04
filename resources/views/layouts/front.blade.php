c7fdfff3c7d5e5503a2ed143838b03f67f26f5e0
Prakash Poudel <prakash@ininepal.com.local>
2020-08-04 14:39:46 +0545

    fixed dynamic css

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -36,18 +36,19 @@
 	<!-- stylesheet -->
 	<link rel="stylesheet" href="{{asset('assets/front/css/rtl/all.css')}}">
 
-    <!--Updated CSS-->
- 	<link rel="stylesheet" href="{{ asset('assets/front/css/rtl/styles.php?color='.str_replace('#','',$gs->colors).'&amp;'.'header_color='.str_replace('#','',$gs->header_color).'&amp;'.'footer_color='.str_replace('#','',$gs->footer_color).'&amp;'.'copyright_color='.str_replace('#','',$gs->copyright_color).'&amp;'.'menu_color='.str_replace('#','',$gs->menu_color).'&amp;'.'menu_hover_color='.str_replace('#','',$gs->menu_hover_color)) }}">
+		<!--Updated CSS-->
+		<link rel="stylesheet"
+			  href="{{ asset('assets/front/css/rtl/styles.php?color='.str_replace('#','',$gs->colors).'&amp;'.'header_color='.str_replace('#','',$gs->header_color).'&amp;'.'footer_color='.str_replace('#','',$gs->footer_color).'&amp;'.'copyright_color='.str_replace('#','',$gs->copyright_color).'&amp;'.'menu_color='.str_replace('#','',$gs->menu_color).'&amp;'.'menu_hover_color='.str_replace('#','',$gs->menu_hover_color)) }}">
 
-@else
+	@else
 
 	<!-- stylesheet -->
-	<link rel="stylesheet" href="{{asset('assets/front/css/all.css')}}">
+		<link rel="stylesheet" href="{{asset('assets/front/css/all.css')}}">
 
-    <!--Updated CSS-->
- 	<link rel="stylesheet" href="{{ asset('assets/front/css/styles.php?color='.str_replace('#','',$gs->colors).'&amp;'.'header_color='.str_replace('#','',$gs->header_color).'&amp;'.'footer_color='.str_replace('#','',$gs->footer_color).'&amp;'.'copyright_color='.str_replace('#','',$gs->copyright_color).'&amp;'.'menu_color='.str_replace('#','',$gs->menu_color).'&amp;'.'menu_hover_color='.str_replace('#','',$gs->menu_hover_color)) }}">
-
-@endif
+		<!--Updated CSS-->
+		{{-- 	<link rel="stylesheet" href="{{ asset('assets/front/css/styles.php?color='.str_replace('#','',$gs->colors).'&amp;'.'header_color='.str_replace('#','',$gs->header_color).'&amp;'.'footer_color='.str_replace('#','',$gs->footer_color).'&amp;'.'copyright_color='.str_replace('#','',$gs->copyright_color).'&amp;'.'menu_color='.str_replace('#','',$gs->menu_color).'&amp;'.'menu_hover_color='.str_replace('#','',$gs->menu_hover_color)) }}">--}}
+		@include('includes.dynamiccss')
+	@endif
 
 
 
